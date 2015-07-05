b915c4a41d95e4fbf479e487db7b9f34877d63c8
Christopher Schnick <c.schnick@monospark.org>
2015-07-06 00:17:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='monospark_sphinx_theme',
+    version=0.0.1,
+    url='https://github.com/monospark/Monospark-Sphinx-Theme',
+    license='MIT',
+    author='Christopher Schnick',
+    author_email='c.schnick@monospark.org',
+    description='Sphinx theme which is used for monospark docs.',
+    packages=['monospark_sphinx_theme'],
+    install_requires=['sphinx >= 1.3.1'],
+)

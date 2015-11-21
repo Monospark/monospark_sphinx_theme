afc3f530ade7cbea5a6f5015297f30fbdaf5b1d6
Christopher Schnick <c.schnick@monospark.org>
2015-11-21 15:07:37 +0100

    Fix some errors in the setup.py file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='monospark_sphinx_theme',
-    version=0.0.1,
+    version='0.0.1',
     url='https://github.com/monospark/monospark_sphinx_theme',
     license='MIT',
     author='Christopher Schnick',
     author_email='c.schnick@monospark.org',
-    description='Sphinx theme which is used for monospark docs.',
+    description='Sphinx theme which is used for Monospark docs.',
     packages=['monospark_sphinx_theme'],
-    install_requires=['sphinx >= 1.3.1'],
+    install_requires=['sphinx >= 1.3.1']
 )

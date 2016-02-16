e29553d53076a2791a69dcfbfaf1093c99f2538c
Christopher Schnick <c.schnick@monospark.org>
2016-02-16 17:51:11 +0100

    Update the python setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='monospark_sphinx_theme',
-    version='0.0.1',
+    version='1.0',
     url='https://github.com/monospark/monospark_sphinx_theme',
     license='MIT',
     author='Christopher Schnick',
@@ -12,9 +12,7 @@ setup(
     package_data={'monospark_sphinx_theme': [
         'theme.conf',
         '*.html',
-        'static/*.css',
-        'static/*.js',
-        'static/*.png'
+        'static/**/*'
     ]},
     install_requires=['sphinx >= 1.3.1']
 )
